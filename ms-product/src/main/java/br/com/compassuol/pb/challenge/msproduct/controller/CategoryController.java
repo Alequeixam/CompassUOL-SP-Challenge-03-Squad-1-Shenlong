cea44321e1e4604f2e3a3493a80c08a873a468d4
Alex Santana <alequeixam@gmail.com>
2023-07-06 12:12:12 -0300

    added full crud with no exception handling

--- ms-product/src/main/java/br/com/compassuol/pb/challenge/msproduct/controller/CategoryController.java
+++ ms-product/src/main/java/br/com/compassuol/pb/challenge/msproduct/controller/CategoryController.java
@@ -2,13 +2,15 @@ package br.com.compassuol.pb.challenge.msproduct.controller;
 
 import br.com.compassuol.pb.challenge.msproduct.entity.Category;
 import br.com.compassuol.pb.challenge.msproduct.payload.CategoryDTO;
+import br.com.compassuol.pb.challenge.msproduct.payload.ProductDTO;
 import br.com.compassuol.pb.challenge.msproduct.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/categories")
@@ -19,8 +21,32 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getAllCategories() {
+        return ResponseEntity.ok().body(categoryService.getAllCategories());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> findProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.findCategoryById(id));
+    }
+
     @PostMapping
     public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO categoryDTO) {
         return new ResponseEntity<>(categoryService.createCategory(categoryDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long id, @Valid @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO categoryResponse = categoryService.updateCategory(id, categoryDTO);
+
+        return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+
+        return new ResponseEntity<>("Category was successfully deleted", HttpStatus.OK);
     }
 }
