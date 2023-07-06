b0bf3f8d84e173b71f94369ebb36771382c414d3
Alex Santana <alequeixam@gmail.com>
2023-07-06 15:47:18 -0300

    added proper responsestatus

--- ms-product/src/main/java/br/com/compassuol/pb/challenge/msproduct/exception/ResourceNotFoundException.java
+++ ms-product/src/main/java/br/com/compassuol/pb/challenge/msproduct/exception/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.compassuol.pb.challenge.msproduct.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     private String resourceName;
     private String fieldName;
