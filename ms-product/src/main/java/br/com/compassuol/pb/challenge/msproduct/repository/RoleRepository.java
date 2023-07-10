bcc1a798029d7581ba36a73f4a3eb92d73488065
Alex Santana <alequeixam@gmail.com>
2023-07-10 16:16:16 -0300

    crud for role

--- ms-product/src/main/java/br/com/compassuol/pb/challenge/msproduct/repository/RoleRepository.java
+++ ms-product/src/main/java/br/com/compassuol/pb/challenge/msproduct/repository/RoleRepository.java
@@ -3,5 +3,8 @@ package br.com.compassuol.pb.challenge.msproduct.repository;
 import br.com.compassuol.pb.challenge.msproduct.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
--- ms-product/src/main/java/br/com/compassuol/pb/challenge/msproduct/service/RoleService.java
+++ ms-product/src/main/java/br/com/compassuol/pb/challenge/msproduct/service/RoleService.java
@@ -1,4 +1,42 @@
 package br.com.compassuol.pb.challenge.msproduct.service;
 
+import br.com.compassuol.pb.challenge.msproduct.entity.Category;
+import br.com.compassuol.pb.challenge.msproduct.entity.Role;
+import br.com.compassuol.pb.challenge.msproduct.exception.ResourceNotFoundException;
+import br.com.compassuol.pb.challenge.msproduct.repository.RoleRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RoleService {
+    private RoleRepository roleRepository;
+    private ModelMapper mapper;
+
+    public RoleService(RoleRepository roleRepository, ModelMapper mapper) {
+        this.roleRepository = roleRepository;
+        this.mapper = mapper;
+    }
+
+    public Role createRole(Role role) {
+
+        List<Role> allRoles = roleRepository.findAll();
+        for (Role r: allRoles) {
+            if (r.getName().equals(role.getName())) {
+                throw new RuntimeException("Role already registered");
+            }
+        }
+        return roleRepository.save(role);
+    }
+
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    public Role getRoleById(Long roleId) {
+        return roleRepository.findById(roleId).orElseThrow(() ->
+                new ResourceNotFoundException("Role", "id", roleId));
+    }
+
 }
